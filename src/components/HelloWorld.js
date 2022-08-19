804c93fc2e81cd439441570d3c49ad308a778083
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-08-19 13:21:59 +0530

    Hello World using arrow function

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react'
 
 class HelloWorldUsingDefault extends Component {
-render() {
-return (
-<div>Hello World! using default export</div>
-)
-}
+render=()=> {
+    return <div>Hello World! using default export</div>}
 }
 class HelloWorldUsingNamed extends Component {
-render() {
-return (
-<div>Hello World! using named export</div>
-)
-}
+render=()=> {
+return <div>Hello World! using named export</div>}
 }
 
 export {HelloWorldUsingNamed}
