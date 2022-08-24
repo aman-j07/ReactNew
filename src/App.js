9728afcaebd6611e91bf9f2c5bd6cfea31b26c94
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-08-24 12:58:51 +0530

    functionalFlipkart

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import First from "./components/First";
 import Second from "./components/Second";
 import Third from "./components/Third";
 import Fourth from "./components/Fourth";
-import Fifth from "./components/Fifth";
-import Sixth from "./components/Sixth";
 function App() {
   return (
     <div className="App">
@@ -12,8 +10,6 @@ function App() {
       <Second />
       <Third />
       <Fourth />
-      <Fifth />
-      <Sixth />
     </div>
   );
 }
--- src/components/Fourth.js
+++ src/components/Fourth.js
@@ -1,68 +1,25 @@
 import style from './Fourth.module.css'
+import pic from './ad.png'
 const Fourth=()=>{
+    const left=">"
     return (
       <div id={style.fourth}>
-        <h1>Pick your Premium</h1>
-        <p>Listen without limits on your phone, speaker and other devices.</p>
-        <div id={style.paymentMethods}>
-             <img alt="null" src="https://i0.wp.com/financebuddha.com/blog/wp-content/uploads/2016/12/23155635/UPI1.jpg?fit=945%2C709&ssl=1"></img>
-             <img alt="null" src="https://upload.wikimedia.org/wikipedia/commons/4/42/Paytm_logo.png"></img>
-             <img alt="null" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Visa.svg/1200px-Visa.svg.png"></img>
-             <img alt="null" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Mastercard_2019_logo.svg/1200px-Mastercard_2019_logo.svg.png"></img>
-             <img alt="null" src="https://cdn-icons-png.flaticon.com/512/179/179431.png"></img>
-             <img alt="null" src="https://1000logos.net/wp-content/uploads/2021/05/Diners-Club-International-logo.png"></img></div>
-        <div id={style.cards}>
-            <div className={style.card}><div id={style.oneTime}>One-time plans available</div>
-              <h3>Mini</h3>
-              <p>From ₹7/day</p><p style={{marginBottom:"14%"}}>1 account</p>
-              <hr></hr>
-              <div className={style.divList}><div className={style.checkList}><i id={style.checkIcon} class="fa-solid fa-check"></i><p>Ad-free music listening on mobile</p></div></div>
-              <button id={style.viewPlans}>VIEW PLANS</button>
-              <a id={style.tAndC}>Terms and conditions apply.</a>
-            </div>
-            <div className={style.card}><div id={style.monthFree}>1 month free</div><div id={style.oneTime}>One-time plans available</div>
-              <h3>Individual</h3>
-              <p>From ₹119/month after offer period</p><p>1 account</p>
-              <hr></hr>
-              <div className={style.divList}>
-                <div className={style.checkList}><i id={style.checkIcon} class="fa-solid fa-check"></i><p>Ad-free music listening</p></div>
-                <div className={style.checkList}><i id={style.checkIcon} class="fa-solid fa-check"></i><p>Group Session</p></div>
-                <div className={style.checkList}><i id={style.checkIcon} class="fa-solid fa-check"></i><p>Download 10K songs/devices on 5 devices</p></div>
-              </div>
-              <button id={style.viewPlans}>VIEW PLANS</button>
-              <div id={style.divTAndC}><a id={style.tAndC}>Terms and conditions apply.</a><p>1 month free not available for users who have already tried Premium</p></div>
-            </div>
-            <div className={style.card}>
-            <div id={style.monthFree}>1 month free</div><div id={style.oneTime}>One-time plans available</div>
-              <h3>Duo</h3>
-              <p>From ₹149/month after offer period</p><p>2 accounts</p>
-              <hr></hr>
-              <div className={style.divList}>
-                <div className={style.checkList}><i id={style.checkIcon} class="fa-solid fa-check"></i><p>For couples who live together</p></div>
-                <div className={style.checkList}><i id={style.checkIcon} class="fa-solid fa-check"></i><p>Ad-free music listening</p></div>
-                <div className={style.checkList}><i id={style.checkIcon} class="fa-solid fa-check"></i><p>Group Session</p></div>
-                <div className={style.checkList}><i id={style.checkIcon} class="fa-solid fa-check"></i><p>Download 10K songs/devices on 5 devices</p></div>
-              </div>
-              <button id={style.viewPlans}>VIEW PLANS</button>
-              <div id={style.divTAndC}><a id={style.tAndC}>Terms and conditions apply.</a><p>1 month free not available for users who have already tried Premium</p></div>
-            </div>
-            <div className={style.card}>
-            <div id={style.monthFree}>1 month free</div><div id={style.oneTime}>One-time plans available</div>
-              <h3>Family</h3>
-              <p>From ₹179/month after offer period</p><p>6 accounts</p>
-              <hr></hr>
-              <div className={style.divList}>
-                <div className={style.checkList}><i id={style.checkIcon} class="fa-solid fa-check"></i><p>For family who live together</p></div>
-                <div className={style.checkList}><i id={style.checkIcon} class="fa-solid fa-check"></i><p>Block explicit music</p></div>
-                <div className={style.checkList}><i id={style.checkIcon} class="fa-solid fa-check"></i><p>Ad-free music listening</p></div>
-                <div className={style.checkList}><i id={style.checkIcon} class="fa-solid fa-check"></i><p>Group Session</p></div>
-                <div className={style.checkList}><i id={style.checkIcon} class="fa-solid fa-check"></i><p>Download 10K songs/devices on 5 devices</p></div>
-              </div>
-              <button id={style.viewPlans}>VIEW PLANS</button>
-              <div id={style.divTAndC}><a id={style.tAndC}>Terms and conditions apply.</a><p>1 month free not available for users who have already tried Premium</p></div>
-            </div>
+        <div id={style.fourthLeft}>
+                <div id={style.divDeal}>Deal of the Day <i id={style.iconTimer} class="fa-solid fa-clock"></i><p id={style.paraTimer}>23 : 10 : 50 Left</p><button id={style.btnView}>VIEW ALL</button></div>
+                     <div id={style.divDeals}>
+                
+                <div><img src="https://rukminim1.flixcart.com/image/150/150/kyag87k0/computer/7/r/u/raider-ge66-12ugs-gaming-laptop-msi-original-imagakb9zw7gqzcm.jpeg?q=70"></img><p id={style.dealName}>Laptops</p><p id={style.dealOffer}>Up to 55% off</p><p id={style.dealMessage}>Top Deals</p></div>
+                <div><img src="https://rukminim1.flixcart.com/image/150/150/printer/k/r/f/canon-mg2470-original-imadqasfqaktjbz5.jpeg?q=70"></img><p id={style.dealName}>Printers</p><p id={style.dealOffer}>Up to 55% off</p><p id={style.dealMessage}>Top Deals</p></div>
+                <div><img src="https://rukminim1.flixcart.com/image/150/150/ks6ef0w0/tripod/tripod/4/d/c/nz-tpd-3110-nainz-original-imag5svcdbqxyas6.jpeg?q=70"></img><p id={style.dealName}>Tripods</p><p id={style.dealOffer}>Up to 55% off</p><p id={style.dealMessage}>Top Deals</p></div>
+                <div><img src="https://rukminim1.flixcart.com/image/150/150/kx50gi80/projector/r/9/p/zeb-pixa-play-12-5-6-dobly-audio-led-projector-zebronics-original-imag9z3yujqmzqt4.jpeg?q=70"></img><p id={style.dealName}>Projectors</p><p id={style.dealOffer}>Up to 55% off</p><p id={style.dealMessage}>Top Deals</p></div>
+                <div><img src="https://rukminim1.flixcart.com/image/150/150/l3uhvgw0/cycle/n/z/c/torro-26t-26-16-montra-72-21-gear-original-imagevzncygdd8ah.jpeg?q=70"></img><p id={style.dealName}>Bicycles</p><p id={style.dealOffer}>Up to 55% off</p><p id={style.dealMessage}>Top Deals</p></div>
+                <div><img src="https://rukminim1.flixcart.com/image/150/150/ksdjma80/microphone/m/d/d/au-400-maono-original-imag5yh9g9nnz7kz.jpeg?q=70"></img><p id={style.dealName}>Microphones</p><p id={style.dealOffer}>Up to 55% off</p><p id={style.dealMessage}>Top Deals</p></div>
+                <div><img src="https://rukminim1.flixcart.com/image/150/150/kx50gi80/pen/h/z/k/119766-flair-original-imag9nzubznagufg.jpeg?q=70"></img><p id={style.dealName}>Stationary</p><p id={style.dealOffer}>Up to 55% off</p><p id={style.dealMessage}>Top Deals</p></div>
+                <button id={style.btnSlide}>{left}</button>
+                <div><img src="https://rukminim1.flixcart.com/image/150/150/kdbzqfk0/bar/8/h/q/push-up-bar-0-8-long-ankaro-original-imafu9dmvdk3rzvy.jpeg?q=70"></img><p id={style.dealName}>Gym Equipments</p><p id={style.dealOffer}>Up to 55% off</p><p id={style.dealMessage}>Top Deals</p></div>
+                </div>
         </div>
-        <div id={style.spldiscount}>Special discount for eligible students in university<button id={style.btnLearnMore}>LEARN MORE</button></div>
+        <div id={style.fourthRight}><img src={pic}></img></div>
       </div>
     )
   }
