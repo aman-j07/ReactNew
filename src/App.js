6612b1af0e26db02857eff9b5863969c7764ab88
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-08-26 11:11:25 +0530

    Image Gallery

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
-import Hello from './components/HelloWorld';
-
+import Product from './components/Product';
+import basketball from './components/images/basketball.png'
+import football from './components/images/football.png'
+import soccer from './components/images/soccer.png'
+import tabletennis from './components/images/table-tennis.png'
+import tennis from './components/images/tennis.png'
 function App() {
-return (
-<div className="App">
-<Hello name="Aman"/>
-</div>
-);
+  return (
+    <div className="App">
+     <Product pic={basketball} name="Basketball" price="$90.00"/>
+     <Product pic={football} name="Rugby Ball" price="$150.00"/>
+     <Product pic={soccer} name="Football" price="$80.00"/>
+     <Product pic={tabletennis} name="Billiards Ball" price="$110.00"/>
+     <Product pic={tennis} name="Tennis Ball" price="$120.00"/>
+    </div>
+  );
 }
 
 export default App;
