369f0e63f92e92fc404b12307a7f9ceec6961b52
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-08-29 18:06:59 +0530

    random

---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './banner.css'
+class banner extends Component {
+  render() {
+    return (
+      <div id='banner'>
+        <h1>Order groceries for delivery or pickup today</h1>
+        <p>Whatever you want from local stores, brought right to your door.</p>
+      </div>
+    )
+  }
+}
+
+export default banner
