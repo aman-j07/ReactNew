a5c1bc586b1dafc081df7083a84d7eebd49969d6
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-08-31 13:27:38 +0530

    Change Theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
+import Themes from "./components/Themes";
 function App() {
   return (
     <div className="App">
+      <Themes/>
     </div>
   );
 }
--- src/components/Themes.js
+++ src/components/Themes.js
@@ -1,15 +1,35 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import "./Light.css"
+import './Dark.css'
+import './Party.css'
 class Themes extends Component {
+  class = "light";
+  id="theme";
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       class:this.class,
+    }
+  }
+  clickHandlerTheme = (event) => {
+    this.id=""
+    this.class = event.target.getAttribute("id");
+    console.log(this.class)
+    this.setState({
+        class:this.class,
+    })
+  };
   render() {
     return (
-      <div>React Themes
-        <buton>Light</buton>
-        <buton>Dark</buton>
-        <buton>Party</buton>
+      <div id="theme" className={this.class} >
+        <h1>React Themes</h1>
+        <button id="light" onClick={this.clickHandlerTheme}>Light</button>
+        <button id="dark" onClick={this.clickHandlerTheme}>Dark</button>
+        <button id="party" onClick={this.clickHandlerTheme}>Party</button>
       </div>
-    )
+    );
   }
 }
 
-export default Themes
+export default Themes;
