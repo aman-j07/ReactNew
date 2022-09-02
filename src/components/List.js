51ff673c31ebe6b314bfe1854ff48bf59b13d547
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-09-02 14:04:04 +0530

    lists 4

--- src/components/List.js
+++ src/components/List.js
@@ -1,76 +1,72 @@
 import React, { Component, createRef } from "react";
 
-export class List extends Component {
-  left = "<";
-  right = ">";
-  refGroup1 = createRef();
-  refGroup2 = createRef();
-  group1=["AJAX", "JQuery", "Javascript", "MooTools", "Prototype", "Doja"];group2=["ASP.net", "C#", "VB.net", "Java", "PHP", "Python"];
-
+class List extends Component {
+  items = [];
+  refItem = createRef();
   constructor(props) {
     super(props);
 
     this.state = {
-      group1:this.group1,
-      group2:this.group2,
+      items: this.items,
+      empty: "",
     };
   }
-  clickHandlerRight = () => {
-    let arrTemp=[];
-    arrTemp=this.refGroup1.current.selectedOptions;
-    for(let i=0;i<arrTemp.length;i++){
-        this.group2.push(arrTemp[i].innerHTML);
-        for(let j=0;j<this.group1.length;j++){
-            if(arrTemp[i].innerHTML===this.group1[j]){
-                this.group1.splice(j,1);
-            }
-        }
+  clickHandlerAdd = () => {
+    let item = this.refItem.current.value;
+    if (item === "") {
+      this.setState({
+        empty: "Please enter a character!",
+      });
+    } else {
+      this.refItem.current.value=""
+      this.items.push(item);
+      console.log(this.items);
+      this.setState({
+        items: this.items,
+        empty: "",
+      });
+      console.log(this.state.items);
     }
-    this.setState({
-        group1:this.group1,
-        group2:this.group2,
-    })
   };
-  clickHandlerLeft = () => {
-    let arrTemp=[];
-    arrTemp=this.refGroup2.current.selectedOptions;
-    for(let i=0;i<arrTemp.length;i++){
-        this.group1.push(arrTemp[i].innerHTML);
-        for(let j=0;j<this.group2.length;j++){
-            if(arrTemp[i].innerHTML===this.group2[j]){
-                this.group2.splice(j,1);
-            }
-        }
-    }
+  clickHandlerDelete = event => {
+    let id=event.target.getAttribute("id")
+    this.items.splice(id,1);
     this.setState({
-        group1:this.group1,
-        group2:this.group2,
-    })
+        items: this.items,
+        empty: "",
+      });
   };
   render() {
     return (
-      <div id="container">
-        <div className="groups">
-          Group 1
-          <select id="group1" size="10" multiple ref={this.refGroup1}>
-            {this.state.group1.map((item) => (
-              <option>{item}</option>
-            ))}
-          </select>
+      <>
+        <div id="items">
+          <div>
+            <input
+              id="item"
+              type="text"
+              placeholder="Enter New Item"
+              ref={this.refItem}
+            />
+            <button id="btnAdd" onClick={this.clickHandlerAdd}>
+              Add
+            </button>
+          </div>
+          {this.state.items.map((item,i) => {
+            return (
+              <>
+                <hr />
+                <div>
+                  <button className="cross" id={i} onClick={this.clickHandlerDelete}>
+                    x
+                  </button>
+                  {item}
+                </div>
+              </>
+            );
+          })}
         </div>
-        <div id="buttons">
-          <button onClick={this.clickHandlerRight}>{this.right}</button>
-          <button onClick={this.clickHandlerLeft}>{this.left}</button>
-        </div>
-        <div className="groups">
-          Group 2
-          <select id="group2" size="10" multiple ref={this.refGroup2}>
-            {this.state.group2.map((item) => (
-              <option>{item}</option>
-            ))}
-          </select>
-        </div>
-      </div>
+        <p id="paraEmpty">{this.state.empty}</p>
+      </>
     );
   }
 }
