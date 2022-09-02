57e2f70864e24e221950d841eeb3a71e36778cfd
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-09-02 13:17:20 +0530

    lists 3

--- src/components/List.js
+++ src/components/List.js
@@ -1,73 +1,76 @@
 import React, { Component, createRef } from "react";
 
-class List extends Component {
-  refId = createRef();
-  refName=createRef();
-  refPrice=createRef();
-  products=[];
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       products:"",
-       empty:"",
-       tabId:"product"
-    }
-  }
-  clickHandlerAdd=()=>{
-    let id=this.refId.current.value;
-    let name=this.refName.current.value;
-    let price=this.refPrice.current.value;
-    if(id===""||name===""||price===""){
-       this.setState({
-        empty:"Please enter all the fields!"
-       })
-    }
-    else{
+export class List extends Component {
+  left = "<";
+  right = ">";
+  refGroup1 = createRef();
+  refGroup2 = createRef();
+  group1=["AJAX", "JQuery", "Javascript", "MooTools", "Prototype", "Doja"];group2=["ASP.net", "C#", "VB.net", "Java", "PHP", "Python"];
 
-        let obj={};
-        obj={
-            id:id,
-            name:name,
-            price:price
-        };
-        this.products.push(obj)
-        console.log(this.products)
-        this.setState({
-            product:this.products,
-            empty:"",
-            tabId:"productVisible"
-        })
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      group1:this.group1,
+      group2:this.group2,
+    };
   }
+  clickHandlerRight = () => {
+    let arrTemp=[];
+    arrTemp=this.refGroup1.current.selectedOptions;
+    for(let i=0;i<arrTemp.length;i++){
+        this.group2.push(arrTemp[i].innerHTML);
+        for(let j=0;j<this.group1.length;j++){
+            if(arrTemp[i].innerHTML===this.group1[j]){
+                this.group1.splice(j,1);
+            }
+        }
+    }
+    this.setState({
+        group1:this.group1,
+        group2:this.group2,
+    })
+  };
+  clickHandlerLeft = () => {
+    let arrTemp=[];
+    arrTemp=this.refGroup2.current.selectedOptions;
+    for(let i=0;i<arrTemp.length;i++){
+        this.group1.push(arrTemp[i].innerHTML);
+        for(let j=0;j<this.group2.length;j++){
+            if(arrTemp[i].innerHTML===this.group2[j]){
+                this.group2.splice(j,1);
+            }
+        }
+    }
+    this.setState({
+        group1:this.group1,
+        group2:this.group2,
+    })
+  };
   render() {
     return (
-      <>
-        <div id="form">
-          <div id="id">
-            <label>Product Id</label>
-            <input type="number" id="idinput" ref={this.refId}/>
-          </div>
-          <div id="name">
-            <label>Product Name</label>
-            <input type="text" id="nameinput" ref={this.refName}/>
-          </div>
-          <div id="price">
-            <label>Product Price</label>
-            <input type="number" id="priceinput" ref={this.refPrice}/>
-          </div>
-          <button onClick={this.clickHandlerAdd}>ADD PRODUCT</button>
+      <div id="container">
+        <div className="groups">
+          Group 1
+          <select id="group1" size="10" multiple ref={this.refGroup1}>
+            {this.state.group1.map((item) => (
+              <option>{item}</option>
+            ))}
+          </select>
         </div>
-        <p id="paraEmpty">{this.state.empty}</p>
-        <table id={this.state.tabId}>
-          <tr>
-            <th>Product Id</th>
-            <th>Product Name</th>
-            <th>Product Price</th>
-          </tr>
-            {this.products.map(item=>{return <tr><td>{item.id}</td><td>{item.name}</td><td>{item.price}</td></tr>})}
-        </table>
-      </>
+        <div id="buttons">
+          <button onClick={this.clickHandlerRight}>{this.right}</button>
+          <button onClick={this.clickHandlerLeft}>{this.left}</button>
+        </div>
+        <div className="groups">
+          Group 2
+          <select id="group2" size="10" multiple ref={this.refGroup2}>
+            {this.state.group2.map((item) => (
+              <option>{item}</option>
+            ))}
+          </select>
+        </div>
+      </div>
     );
   }
 }
