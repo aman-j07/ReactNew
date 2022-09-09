63763bd7c201ca5581119038775cb44da1632fd3
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-09-09 17:56:01 +0530

    Restaurant App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
 import SignLog from "./components/SignLog";
-import Restaurants from "./components/Restaurants";
 function App() {
   return (
     <div className="container">
       <SignLog/>
-      {/* <Restaurants/> */}
     </div>
   );
 }
