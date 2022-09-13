63934f0cb95b1394813bb13d44a392475f610133
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-09-13 12:35:42 +0530

    Router 4

--- src/App.js
+++ src/App.js
@@ -6,29 +6,9 @@ import Login from "./components/Login";
 import { useState } from "react";
 let id="navBar"
 function App() {
-  const arrUsers=[{name:"Aman",email:"aman@gmail.com",username:"Aman7",password:"Pass@1234",phone:"1234567890",country:"India",city:"Lucknow",pincode:"226001"}];
-  const [ArrUsers,setArrUsers]=useState(arrUsers);
-  const[Id,setId]=useState(id)
-
-  const changeId=()=>{
-    if(id=="navBar"){
-      id="hidden";
-    }
-    else if(id=="hidden"){
-      id="navBar"
-    }
-    setId(id)
-  }
-
-  const push=(obj)=>{
-    arrUsers.push(obj)
-    setArrUsers([...arrUsers])
-    console.log(arrUsers)
-  }
-
   return (
     <div className="container">
-      <Layout id={Id}/>
+      <Layout/>
         <Routes>
            <Route path="/" element={<Home clickHandler={push} arrUsers={ArrUsers}/>} />
            <Route path="/login" element={<Login  clickHandler={changeId} arrUsers={ArrUsers}/>} />
