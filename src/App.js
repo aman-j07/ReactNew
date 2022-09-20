ead12a8acee2a5f1690953db82920f5031ee3b7e
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-09-20 17:57:14 +0530

    Color Picker

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import ColorPickerGfg from './components/ColorPicker';
-import ElectricBill from './components/ElectricBill';
 function App() {
   return (
     <div className="App">
